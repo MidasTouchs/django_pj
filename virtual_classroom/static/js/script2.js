12f4ffaea93604cea26cab406c1277c8b8a6e56f
MidasTouchs <140983848+MidasTouchs@users.noreply.github.com>
2025-06-15 17:26:31 +0100

    Delete virtual_classroom directory

--- virtual_classroom/static/js/script2.js
+++
@@ -1,7 +0,0 @@
-
-function toggleDarkMode() {
-  const body = document.body;
-  const toggle = document.getElementById('toggle-btn');
-  body.classList.toggle('dark');
-  toggle.classList.toggle('active');
-}
